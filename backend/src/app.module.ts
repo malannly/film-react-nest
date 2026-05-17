2770ace3a288fd5d2d03f0b74aa9a86a7f5f4741
malannly <mamedovaanna.i@gmail.com>
2026-05-17 09:31:40 +0300

    сдача проекной работы

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as path from 'node:path';
 
 import { FilmsModule } from './films/films.module';
@@ -11,15 +11,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
 
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || '127.0.0.1',
-      port: parseInt(process.env.DB_PORT || '5432'),
-      username: process.env.DB_USERNAME || 'prac',
-      password: process.env.DB_PASSWORD || '1234',
-      database: process.env.DB_NAME || 'prac',
-      autoLoadEntities: true,
-      synchronize: false,
+  TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const dbUrl = new URL(
+          config.get<string>('DATABASE_URL') || 'postgres://localhost:5432/films',
+        );
+        return {
+          type: 'postgres',
+          host: dbUrl.hostname,
+          port: parseInt(dbUrl.port || '5432'),
+          database: dbUrl.pathname.slice(1),
+          username: config.get<string>('DATABASE_USERNAME') || 'postgres',
+          password: config.get<string>('DATABASE_PASSWORD') || 'postgres',
+          autoLoadEntities: true,
+          synchronize: false,
+        };
+      },
     }),
 
     FilmsModule,
